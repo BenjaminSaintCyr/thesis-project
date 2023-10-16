fb46e6a2709eea8b158fcf22c6a77806717c3776
Benjamin Saint-Cyr <44852962+BenjaminSaintCyr@users.noreply.github.com>
2023-10-16 15:19:59 -0400

    add multi-node

---
+++ multi-node/scripts/versions.bash
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# https://storage.googleapis.com/kubernetes-release/release/stable.txt
+readonly k8s_version="v1.27.3"
+readonly etcd_version="v3.4.14"
+readonly cfssl_version="1.5.0"
+readonly traefik_version="v2.4.0"
+readonly crio_version="v1.26.4"
+readonly conmon_version="v2.0.25"
+readonly cni_plugins_version="v0.9.0"
+readonly runc_version="v1.0.0-rc92"
